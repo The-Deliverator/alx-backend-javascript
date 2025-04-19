f3ec1d393ed6ee69aa22f3c7fcd61c726f41a684
The-Deliverator <mdavoo@gmail.com>
2025-04-19 03:18:21 +0000

    6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,12 +4,10 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]).then((values) => {
-    const arr = [];
-    for (const item of values) {
-      arr.push({ status: item.status, value: item.value || item.reason });
-    }
-    return arr;
-  });
+    uploadPhoto(fileName)
+  ])
+    .then((results) => results.map(({ status, value, reason }) => ({
+      status,
+      value: value || reason,
+    })));
 }
