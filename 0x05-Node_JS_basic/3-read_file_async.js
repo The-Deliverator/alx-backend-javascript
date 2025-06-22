0fb51aa5ab1688c04135fa95bbee21488299b2c0
The-Deliverator <mdavoo@gmail.com>
2025-06-22 20:23:08 +0000

    3-read_file_async.js

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,35 +1,38 @@
 const fs = require('fs');
 
-function countStudents(path) {
+/**
+ * Reads CSV file asynchronously, parses the data and
+ * prints it to stdout.
+ * @param {string} dbPath - path to csv file.
+ */
+function countStudents(dbPath) {
   return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf8', (err, data) => {
-      if (err) {
-        reject(new Error('Cannot load the database'));
-        return;
+    fs.readFile(dbPath, 'utf-8', (error, data) => {
+      if (error) {
+        reject(Error('Cannot load the database'));
+      } else {
+        const courses = new Map();
+        let students = data.split('\n');
+        students = students.slice(1, students.length - 1);
+
+        // Parse CSV data creating a map of courseData objects.
+        students.forEach((student) => {
+          const studentData = student.split(',');
+          const firstName = studentData[0];
+          const field = studentData[3];
+          if (courses.has(field)) courses.get(field).push(firstName);
+          else courses.set(field, [firstName]);
+        });
+
+        // Display information from map
+        console.log(`Number of students: ${students.length}`);
+        courses.forEach((courseStudents, course) => {
+          console.log(`Number of students in ${course}: ${courseStudents.length}. List: ${courseStudents.join(', ')}`);
+        });
+        resolve();
       }
-
-      const lines = data.trim().split('\n').filter(line => line);
-      const students = lines.slice(1); // Skip header
-
-      const fields = {};
-
-      students.forEach((line) => {
-        const [firstname, , , field] = line.split(',');
-        if (!fields[field]) {
-          fields[field] = [];
-        }
-        fields[field].push(firstname);
-      });
-
-      console.log(`Number of students: ${students.length}`);
-      for (const [field, names] of Object.entries(fields)) {
-        console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
-      }
-
-      resolve(); // Done logging
     });
   });
 }
 
 module.exports = countStudents;
-
