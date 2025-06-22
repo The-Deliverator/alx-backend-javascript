631a708e9955004399951d03a7f9059cab7d7ec1
The-Deliverator <mdavoo@gmail.com>
2025-06-22 20:13:54 +0000

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,21 @@
-process.stdout.write('Welcome to ALX School, what is your name?\n');
+/**
+ * Interactive script to print user's name
+ *  - Prompts user to enter user name
+ *  - Display entered user name
+ *  - Notifies user when exiting
+ */
 
-process.stdin.on('data', (data) => {
-  process.stdout.write(`Your name is: ${data}`);
+const { stdout, stdin } = process;
+
+stdout.write('Welcome to Holberton School, what is your name?\n');
+
+stdin.setEncoding('utf-8');
+stdin.on('readable', () => {
+  const name = stdin.read();
+  stdout.write(`Your name is: ${name}`);
+  process.exit();
 });
 
-process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
+process.on('exit', () => {
+  stdout.write('This important software is now closing\n');
 });
