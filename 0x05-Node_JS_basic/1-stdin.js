8aea425cf940d7cae6f90f66494dd09174773bc7
The-Deliverator <mdavoo@gmail.com>
2025-06-22 20:10:05 +0000

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,9 @@
-process.stdout.write('Welcome to ALX, what is your name?\n');
+process.stdout.write('Welcome to ALX School, what is your name?\n');
 
-process.stdin.on('data', (input) => {
-  const name = input.toString().trim();
-  console.log(`Your name is: ${name}`);
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data}`);
 });
 
-// When the user exits (CTRL+D)
 process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
-
